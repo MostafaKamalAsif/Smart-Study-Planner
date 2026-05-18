0d34d15a6402487dd4a57be6e3b784645a3c0847
=-= Torikus Sadik =-= <98456545+0xsadik@users.noreply.github.com>
2026-05-18 11:58:27 +0600

    Add Assignments component to dashboard and importing dependencis

---
+++ src/app/(dashboard)/assignments/Assignments.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  Plus,
+  Clock,
+  CheckCircle2,
+  Edit2,
+  Trash2,
+} from "lucide-react";
+import { Card } from "@/components/ui/Card";
+import { Button } from "@/components/ui/Button";
+import { Badge } from "@/components/ui/Badge";
+import { Input } from "@/components/ui/Input";
+import { Modal } from "@/components/ui/Modal";
+import { PageTransition } from "@/components/ui/PageTransition";
+import { MOCK_ASSIGNMENTS, MOCK_SUBJECTS } from "@/lib/constants";
+import { Status } from "@/types";
+
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
