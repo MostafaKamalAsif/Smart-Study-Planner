78379d76c2830f9887efa28ed1ee18c18c38cfd7
Md Zobayer Hossain Mamun <zobayerhossainmamun@gmail.com>
2026-05-21 20:45:03 +0600

    fix font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: " Smart Study Planner",
@@ -13,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
